2084ae592961c73a21e4476c9e0eb60a476f6073
Latife Dag <latifedag0@gmail.com>
2022-05-04 22:39:27 +0200

    Learn float and double

---
+++ PrimitiveTypesChallenge/FloatAndDouble/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        float floatMin = Float.MIN_VALUE;
+        float floatMax = Float.MAX_VALUE;
+        System.out.println(" min float " + floatMin);
+        System.out.println(" max float " + floatMax);
+
+        double doubleMin = Double.MIN_VALUE;
+        double doubleMax = Double.MAX_VALUE;
+        System.out.println(" min double " + doubleMin);
+        System.out.println(" max double " + doubleMax);
+
+        int intV = 5;
+        float floatV = (float) 5.25;
+        double doubleV = 5.25d;
+    }
+}
