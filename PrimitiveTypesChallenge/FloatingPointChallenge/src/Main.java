bdf5cea35975e238128234efb18b3f26f322aedb
Latife Dag <latifedag0@gmail.com>
2022-05-11 10:07:51 +0200

    Floating point challenge

---
+++ PrimitiveTypesChallenge/FloatingPointChallenge/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double pound = 0.45359237;
+        double kg = 3;
+        kg = pound * kg;
+        System.out.println(" 3 kg is = " + kg + " pounds. ");
+
+        double numberOfPounds = 200d;
+        double convertedKilograms = numberOfPounds * 0.45359237;
+        System.out.println(" converted kg = " + convertedKilograms);
+
+    }
+}
