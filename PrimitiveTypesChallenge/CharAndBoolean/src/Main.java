739a42b024275718076f6dfdb443d02444152d62
Latife Dag <latifedag0@gmail.com>
2022-05-12 17:52:03 +0200

    Char and Boolean

---
+++ PrimitiveTypesChallenge/CharAndBoolean/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char uniChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(uniChar);
+        char copyright = '\u00A9';
+        System.out.println(copyright);
+        boolean ValueTrue = true;
+        boolean ValueFalse = false;
+
+
+
+    }
+}
